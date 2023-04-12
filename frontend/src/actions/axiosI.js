0c2fd7bf442b12bfb95fe06ad77ca76d6fbbcb2c
Shvet Kantibhai Anaghan <sh618812@dal.ca>
2023-04-12 18:10:38 -0300

    c14

--- frontend/src/actions/axiosI.js
+++ frontend/src/actions/axiosI.js
@@ -1,6 +1,5 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  // baseURL: "http://localhost:7001",
-  baseURL: process.env.REACT_APP_SERVER,
+  baseURL: `${process.env.REACT_APP_SERVER}`,
 });
